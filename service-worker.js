eacb8d4a018d6f6c39d6530868ba8017ace791ef
felipekeller08 <felipeborile@gmail.com>
2025-08-27 04:10:11 -0300

    app

---
+++ service-worker.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = "mybank-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js",
+  "/img/logo192.png",
+  "/img/logo512.png"
+];
+
+// Instala o service worker e guarda os arquivos no cache
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Ativa o SW limpando caches antigos
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Intercepta as requisições e serve do cache
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
